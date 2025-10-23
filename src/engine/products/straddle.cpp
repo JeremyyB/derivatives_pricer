f8232267eacc6d05d887d1c4835dfd1c514c6ffe
Jeremy B <157044310+JeremyyB@users.noreply.github.com>
2025-10-23 20:36:04 +0200

    Fix Straddle class' code location

--- src/engine/products/straddle.cpp
+++ src/engine/products/straddle.cpp
@@ -1,9 +1,3 @@
 #include "straddle.h"
 
 
-class Straddle : public Strangle  {
-public:
-    Straddle(double s0, double strike, double time_to_maturity, double position, const std::string &underlying_id)
-        : Strangle(s0, time_to_maturity, position, underlying_id, strike, strike) {
-    }
-};
