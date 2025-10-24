f4cb9a485250c775da15c64a188bd592c6c2520a
Jeremy B <157044310+JeremyyB@users.noreply.github.com>
2025-10-24 15:58:04 +0200

    Add Binomial and Monte-Carlo implementation of Strangle pricing

--- src/engine/products/strangle.cpp
+++ src/engine/products/strangle.cpp
@@ -1,6 +1,8 @@
 #include "strangle.h"
 
+#include "engine/models/Binomial.h"
 #include "engine/models/BlackScholes.h"
+#include "engine/models/MonteCarlo.h"
 
 double Strangle::payoff(double S1) {
     return (std::max(Kput - S1, 0.) + std::max(S1 - Kcall, 0.)) * position;
@@ -9,8 +11,13 @@ double Strangle::payoff(double S1) {
 double Strangle::price(double vol, double riskfree_rate, const std::shared_ptr<PricingParams> &methodsParams) {
     double res = -1;
     switch (methodsParams->getMethodType()) {
-        case MTE_CARLO:
-            // TODO
+        case MTE_CARLO: {
+            std::shared_ptr<MonteCarloParams> pricing_params = std::dynamic_pointer_cast<MonteCarloParams>(methodsParams);
+            MonteCarlo model = MonteCarlo(pricing_params->steps, pricing_params->simulations, pricing_params->seed);
+            double call_price = model.priceEUCall(S0, Kcall, timeToMaturity, riskfree_rate, vol);
+            double put_price = model.priceEUPut(S0, Kput, timeToMaturity, riskfree_rate, vol);
+            res = call_price + put_price;
+        }
             break;
         case BLACK_SCHOLES: {
             BlackScholes model = BlackScholes();
@@ -19,8 +26,13 @@ double Strangle::price(double vol, double riskfree_rate, const std::shared_ptr<P
             res = call_price + put_price;
         }
         break;
-        case BINOMIAL:
-            // TODO
+        case BINOMIAL: {
+            std::shared_ptr<BinomialParams> pricing_params = std::dynamic_pointer_cast<BinomialParams>(methodsParams);
+            Binomial model = Binomial(pricing_params->steps, pricing_params->u, pricing_params->d);
+            double call_price = model.priceEUCall(S0, Kcall, timeToMaturity, riskfree_rate, vol);
+            double put_price = model.priceEUPut(S0, Kput, timeToMaturity, riskfree_rate, vol);
+            res = call_price + put_price;
+        }
             break;
     }
     return position * res;
