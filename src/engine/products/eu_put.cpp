3784972f2b4e27f50c678275e434a61b14a36c3b
Jeremy B <157044310+JeremyyB@users.noreply.github.com>
2025-10-24 15:46:02 +0200

    Add Binomial and Monte-Carlo implementation of EUCall pricing

--- src/engine/products/eu_put.cpp
+++ src/engine/products/eu_put.cpp
@@ -2,7 +2,9 @@
 
 #include <algorithm>
 
+#include "engine/models/Binomial.h"
 #include "engine/models/BlackScholes.h"
+#include "engine/models/MonteCarlo.h"
 
 double EUPut::payoff(double S1) {
     return std::max(strike - S1, 0.) * position;
@@ -11,16 +13,22 @@ double EUPut::payoff(double S1) {
 double EUPut::price(double vol, double riskfree_rate, const std::shared_ptr<PricingParams> &methodsParams) {
     double res = -1;
     switch (methodsParams->getMethodType()) {
-        case MTE_CARLO:
-            // TODO
+        case MTE_CARLO: {
+            std::shared_ptr<MonteCarloParams> pricing_params = std::dynamic_pointer_cast<MonteCarloParams>(methodsParams);
+            MonteCarlo model = MonteCarlo(pricing_params->steps, pricing_params->simulations, pricing_params->seed);
+            res = model.priceEUPut(S0, strike, timeToMaturity, riskfree_rate, vol);
+        }
             break;
         case BLACK_SCHOLES: {
             BlackScholes model = BlackScholes();
             res = model.priceEUPut(S0, strike, timeToMaturity, riskfree_rate, vol);
         }
             break;
-        case BINOMIAL:
-            // TODO
+        case BINOMIAL: {
+            std::shared_ptr<BinomialParams> pricing_params = std::dynamic_pointer_cast<BinomialParams>(methodsParams);
+            Binomial model = Binomial(pricing_params->steps, pricing_params->u, pricing_params->d);
+            res = model.priceEUPut(S0, strike, timeToMaturity, riskfree_rate, vol);
+        }
             break;
     }
     return position * res;
